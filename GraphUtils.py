abefc707142f83a79cf0be4b6c81779d5fa1f841
Balachandran A R <chandran1993bala@gmail.com>
2017-09-02 21:42:26 +0530

    Modularised the code a little bit. Tried the KivyApp for UI. But, currently not using it because of some error between Kivy and MatPlotLib

--- GraphUtils.py
+++ GraphUtils.py
@@ -5,13 +5,11 @@ import matplotlib.ticker as mticker
 
 
 def plot_time_series(x_date_obj_array, y_values, y_label, y_values_prefix, date_format='%d %b %Y'):
-
     for i,_ in enumerate(x_date_obj_array):
         x_date_obj_array[i] = mdates.date2num(x_date_obj_array[i])
 
     fig, ax = plt.subplots()
     ax = sns.tsplot(data=y_values, time=x_date_obj_array, value=y_label, ax=ax)
-
     # assign locator and formatter for the xaxis ticks.
     ax.xaxis.set_major_locator(mdates.AutoDateLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-import FAN_Abs
-import TimeUtils
-import GraphUtils
-import time
-import sys
 
+import sys
+import FANHelper
+
+# This code is used to get the FAN APP ID and FAN ACCESS TOKEN from the command line arguments
 if len(sys.argv) == 3:
 
     fan_app_id = sys.argv[1]
     fan_access_token = sys.argv[2]
 
-    fan_res = FAN_Abs.fan_api_request(fan_app_id, fan_access_token, 1496793600, int(time.time()))
-
-    if fan_res.status_code == 200:
-        data = fan_res.json()['data']
-        y_values = []
-        x_timeArray = []
-        for datum in data:
-            x_timeArray.append(TimeUtils.fb_report_date_to_datetime(datum['time']))
-            y_values.append(float(datum['value']))
-
-        GraphUtils.plot_time_series(x_timeArray, y_values, "Revenue","$")
-    else:
-        print("Bad Response. Code :" + str(fan_res.status_code))
-
+    FANHelper.get_and_plot_fan_data(fan_app_id, fan_access_token)
 else:
     print("Invalid Command Line Arguments.\nThe arguments should be : <FAN_APP_ID> <FAN_ACCESS_TOKEN>")
+
+
